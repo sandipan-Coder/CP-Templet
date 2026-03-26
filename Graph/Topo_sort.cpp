250b0e3ef72b0355149165e836b4d613d613e684
sandipan-Coder <palsandipan68@gmail.com>
2026-03-26 09:08:33 +0530

    Add Graph section

---
+++ Graph/Topo_sort.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> topological_sort(int V, vector<int> adj[]) {
+
+    vector<int> indegree(V, 0);
+    vector<int> ans;
+
+    for(int i = 0; i < V; i++) {
+        for(auto it: adj[i])
+            indegree[it]++;
+    }
+
+    queue<int> q;
+    for(int i = 0; i < V; i++) {
+        if(indegree[i] == 0)
+            q.push(i);
+    }
+
+    while(!q.empty()) {
+
+        int node = q.front();
+        q.pop();
+        ans.push_back(node);
+
+        for(auto it: adj[node]) {
+            indegree[it]--;
+
+            if(indegree[it] == 0)
+                q.push(it);
+        }
+    }
+
+    return ans;
+}
